caca7cf60d4f12ae3f00852a63f326e3bf1f75a5
primaska12 <63790558+primaska12@users.noreply.github.com>
2026-05-31 22:19:38 +0700

    Delete patch8.cjs

--- patch8.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/Home.tsx', 'utf8');
-
-code = code.replace(
-  'h-[52px] flex items-center justify-center gap-2 bg-slate-800',
-  'h-[42px] sm:h-[52px] flex items-center justify-center gap-2 bg-slate-800'
-);
-
-fs.writeFileSync('src/Home.tsx', code);
